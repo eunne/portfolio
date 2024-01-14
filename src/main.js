00e91ea73860d5b7258a630b247bce7dd8637465
eunne <85356011+eunne@users.noreply.github.com>
2024-01-14 15:41:39 +0900

    [JS] header dark styling when scroll down

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// window : 글로벌 객체, 브라우저를 의미함
+// window.document.querySelector();
+
+
+// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
+// [how to] clinetY 좌표 값 > header height -> header 색상을 black으로 바꾼다.black으로
+
+// ? header height는 어떻게 알 수 있지?
+const header = document.querySelector('.header');
+const headerHeight =  header.getBoundingClientRect().height;
+
+// ? 페이지가 스크롤링 될 때, clientY의 좌표값을 읽는다. 어떻게?
+// window창 자체에서 scroll event가 일어나므로..
+// console.log(window.scrollY);
+
+//!구현
+document.addEventListener('scroll', () => {
+  if (headerHeight < window.scrollY) {
+    // classlist에 새로운 class를 추가해주기
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+})
